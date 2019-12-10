5333a4701a65f037f3ed5da8349d499e44184128
Davide Porrovecchio <pdavide@gmail.com>
2019-12-10 21:07:49 +0100

    Initial commit

---
+++ LoginFilterIp.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * Matomo - free/libre analytics platform
+ * Plugin developed for Web Analytics Italia (https://webanalytics.italia.it)
+ *
+ * Some code is reused from Matomo Piwik\Plugins\CoreHome\LoginWhitelist.
+ *
+ * @link https://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+namespace Piwik\Plugins\LoginFilterIp;
+
+use Exception;
+use Piwik\Config;
+use Piwik\IP;
+use Piwik\Network\IP as NetworkIp;
+use Piwik\Piwik;
+use Piwik\Url;
+
+class LoginFilterIp extends \Piwik\Plugin
+{
+    /**
+     * The configuration array.
+     *
+     * @var array
+     */
+    protected $pluginConfig;
+
+    /**
+     * Construct a new LoginFilterIp instance.
+     */
+    public function __construct() {
+        parent::__construct();
+
+        $this->pluginConfig = Config::getInstance()->{$this->pluginName};
+    }
+
+    /**
+     * Get event handlers.
+     *
+     * @return array the event handlers
+     */
+    public function registerEvents(): array
+    {
+        return [
+            'Controller.Login.' => 'loginFilterIP',
+            'Controller.Login.index' => 'loginFilterIP',
+            'Controller.Login.confirmResetPassword' => 'loginFilterIP',
+            'Controller.Login.confirmPassword' => 'loginFilterIP',
+            'Controller.Login.resetPassword' => 'loginFilterIP',
+            'Controller.Login.login' => 'loginFilterIP',
+        ];
+    }
+
+    /**
+     * Filter IP using the value of the client IP address.
+     */
+    public function loginFilterIP()
+    {
+        $this->checkIsAllowedLogin(IP::getIpFromHeader());
+    }
+
+    /**
+     * Send a response based on the passed client IP address.
+     *
+     * @param string $ip the address to check
+     * 
+     * @throws Exception if the passed IP address is not allowed and redirect is not configured
+     */
+    protected function checkIsAllowedLogin(string $ip)
+    {
+        if (!$this->isAllowedLogin($ip)) {
+            if ($this->shouldRedirectUnallowedIPs()) {
+                Url::redirectToUrl($this->pluginConfig['redirect_unallowed_to']);
+
+                return;
+            }
+            throw new Exception(Piwik::translate('CoreHome_ExceptionNotWhitelistedIP', $ip));
+        }
+    }
+
+    /**
+     * Check if the passed IP address is allowed to login.
+     *
+     * @param string $userIp the address to check
+     * 
+     * @return bool whether the passed IP address is not allowed to login
+     */
+    protected function isAllowedLogin(string $userIp): bool
+    {
+        $userIp = NetworkIp::fromStringIP($userIp);
+        $allowedIPs = $this->getAllowedIPs();
+
+        if (empty($allowedIPs)) {
+            return false;
+        }
+
+        return $userIp->isInRanges($allowedIPs);
+    }
+
+    /**
+     * Get the list of the allowed IPs/ranges.
+     * 
+     * @return array the list of the allowed IPs/ranges
+     */
+    protected function getAllowedIPs(): array
+    {
+        if (!is_array($this->pluginConfig['allow_login_from'] ?? null)) {
+            return [];
+        }
+
+        $allowedIPs = array_map(function ($allowedIP) {
+            return trim($allowedIP);
+        }, $this->pluginConfig['allow_login_from']);
+
+        return array_unique(array_values(array_filter($allowedIPs, function ($allowedIP) {
+            return !empty($allowedIP);
+        })));
+    }
+
+    /**
+     * Check whether the unallowed IPs should be redirected.
+     * 
+     * @return bool whether the unallowed IPs should be redirected
+     */
+    protected function shouldRedirectUnallowedIPs(): bool
+    {
+        if (!is_string($this->pluginConfig['redirect_unallowed_to'] ?? null)) {
+            return false;
+        }
+
+        return false !== filter_var($this->pluginConfig['redirect_unallowed_to'], FILTER_VALIDATE_URL);
+    }
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+# Matomo LoginFilterIp Plugin
+
+## Description
+
+This plugin makes possible to filter (whitelist) the ip/ranges allowed to
+login in your Matomo instance with username/password credentials.
+
+This is different from the [whitelisting
+feature](https://matomo.org/faq/how-to/faq_25543/) included in Matomo because
+the filter is only applied to the login action when using username and password
+and not when providing a single sign-on experience using the [logme
+action](https://matomo.org/faq/how-to/faq_30/) provided by the Login plugin.
+
+In this scenario is possibile to completely hide the login form and return an
+error page or redirect the user to another address.
+
+## Usage
+
+Add the following section to your `config.ini.php`:
+
+```
+[LoginFilterIp]
+allow_login_from[] = <IP ADDRESS or RANGE>
+; uncomment to redirect the user instead of displaying an error page
+;redirect_unallowed_to = <URL>
+```
+
+**Make sure you have direct access to the `config.ini.php` file before using this
+plugin as it could prevent you from authenticating in your Matomo instance.**
